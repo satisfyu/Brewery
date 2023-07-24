a3904892bd233922ee489a2ac1df32d6d585105a
IamJannik <104758303+IamJannik@users.noreply.github.com>
2023-07-24 20:18:28 +0200

    Revert "1.19.2"

--- common/src/main/java/net/bmjo/brewery/registry/ObjectRegistry.java
+++ common/src/main/java/net/bmjo/brewery/registry/ObjectRegistry.java
@@ -1,29 +1,24 @@
 package net.bmjo.brewery.registry;
 
-import dev.architectury.registry.CreativeTabRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
 import dev.architectury.registry.registries.Registrar;
 import dev.architectury.registry.registries.RegistrySupplier;
 import net.bmjo.brewery.Brewery;
 import net.bmjo.brewery.item.Breathalyzer;
 import net.bmjo.brewery.util.BreweryIdentifier;
-import net.minecraft.core.Registry;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class ObjectRegistry {
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Brewery.MOD_ID,  Registry.ITEM_REGISTRY);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(Brewery.MOD_ID, Registries.ITEM);
     public static final Registrar<Item> ITEM_REGISTRAR = ITEMS.getRegistrar();
-    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Brewery.MOD_ID, Registry.BLOCK_REGISTRY);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(Brewery.MOD_ID, Registries.BLOCK);
     public static final Registrar<Block> BLOCK_REGISTRAR = BLOCKS.getRegistrar();
-    public static final CreativeModeTab VINERY_TAB = CreativeTabRegistry.create(new BreweryIdentifier("brewery"), () ->
-            new ItemStack(ObjectRegistry.BREATHALYZER.get()));
 
     public static final RegistrySupplier<Item> BREATHALYZER = registerI("breathalyzer", () -> new Breathalyzer(getSettings()));
 
@@ -43,7 +38,7 @@ public class ObjectRegistry {
     }
 
     private static Item.Properties getSettings(Consumer<Item.Properties> consumer) {
-        Item.Properties settings = new Item.Properties().tab(VINERY_TAB);
+        Item.Properties settings = new Item.Properties();
         consumer.accept(settings);
         return settings;
     }
